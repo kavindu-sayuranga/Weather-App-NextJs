f87f89326352de5fa4504950afdc0bdf100d7b14
kavindu-sayuranga <ksayuranga00@gmai.com>
2023-05-18 12:53:17 +0530

    API call

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,18 +12,18 @@ export default function Home() {
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
 
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
-  // const fetchWeather = (e) => {
-  //   e.preventDefault();
-  //   setLoading(true);
-  //   axios.get(url).then((response) =>{
-  //     setWeather(response.data);
-  //     // console.log(response.data)
-  //   })
-  //   setCity('');
-  //   setLoading(false)
-  // }
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) =>{
+      setWeather(response.data);
+      // console.log(response.data)
+    })
+    setCity('');
+    setLoading(false)
+  }
 
   
   return (
