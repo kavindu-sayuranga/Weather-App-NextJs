9a8bddd5e9121d02369baccca257f942f469cde7
kavindu-sayuranga <ksayuranga00@gmai.com>
2023-05-18 12:54:46 +0530

    add background image

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,16 +36,16 @@ export default function Home() {
 
         {/* Overlay */}
 
-        {/* <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[1]' /> */}
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[1]' />
 
 
         {/* Background image */}
 
-        {/* <Image src='https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80' 
+        <Image src='https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80' 
         alt="background image"
         layout='fill'
         className="object-cover"
-        /> */}
+        />
 
         {/* Search */}
 
